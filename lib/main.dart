34e988fa89cc032b53c0e9775c8ec293c935b356
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-02 18:42:13 -0500

    create a function playSound which accepts an integer as an argument

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,64 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum){
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-            child: Center(
-              child: FlatButton(
+            child: Column(
+              children: <Widget>[
+               FlatButton(
+                 color: Colors.deepPurple,
           onPressed: () {
-              final player = AudioCache();
-              player.play('note1.wav');
+               playSound(1);
           },
-          child: Text('Click me!'),
         ),
-            )),
+                FlatButton(
+                    color: Colors.blue,
+                  onPressed: () {
+                    playSound(2);
+                  },
+                ),
+                FlatButton(
+                    color: Colors.lightBlueAccent,
+                  onPressed: () {
+                    playSound(3);
+                  },
+                ),
+                FlatButton(
+                    color: Colors.green,
+                  onPressed: () {
+                    playSound(4);
+                  },
+                ),
+                FlatButton(
+                    color: Colors.yellow,
+                  onPressed: () {
+                    playSound(5);
+                  },
+                ),
+                FlatButton(
+                    color: Colors.orange,
+                  onPressed: () {
+                    playSound(6);
+                  },
+                ),
+                FlatButton(
+                  color:Colors.red,
+                  onPressed: () {
+                    playSound(7);
+                  },
+                ),
+
+              ],
+            ),
+        ),
       ),
     );
   }
