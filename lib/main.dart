d41dc16bc530f5175f90d446ad48cedb14ec249a
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-03 00:31:33 -0500

    add an appbar and contain all the buttons in expanded

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,87 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(int soundNum){
+  void playSound(int soundNum) {
     final player = AudioCache();
     player.play('note$soundNum.wav');
-
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(title: Center(child: Text('Xylophone')),backgroundColor: Colors.blueGrey.shade500,),
         body: SafeArea(
-            child: Column(
-              children: <Widget>[
-               FlatButton(
-                 color: Colors.deepPurple,
+      child: Column(
+
+        crossAxisAlignment:CrossAxisAlignment.stretch,
+      children: <Widget>[
+      Expanded(
+        child: FlatButton(
+
+        color: Colors.deepPurple,
           onPressed: () {
-               playSound(1);
+            playSound(1);
           },
         ),
-                FlatButton(
-                    color: Colors.blue,
-                  onPressed: () {
-                    playSound(2);
-                  },
-                ),
-                FlatButton(
-                    color: Colors.lightBlueAccent,
-                  onPressed: () {
-                    playSound(3);
-                  },
-                ),
-                FlatButton(
-                    color: Colors.green,
-                  onPressed: () {
-                    playSound(4);
-                  },
-                ),
-                FlatButton(
-                    color: Colors.yellow,
-                  onPressed: () {
-                    playSound(5);
-                  },
-                ),
-                FlatButton(
-                    color: Colors.orange,
-                  onPressed: () {
-                    playSound(6);
-                  },
-                ),
-                FlatButton(
-                  color:Colors.red,
-                  onPressed: () {
-                    playSound(7);
-                  },
-                ),
-
-              ],
-            ),
+      ),
+      Expanded(child:FlatButton(
+        color: Colors.blue,
+        onPressed: () {
+          playSound(2);
+        },
+      ),),
+      Expanded(
+        child: FlatButton(
+          color: Colors.lightBlueAccent,
+          onPressed: () {
+            playSound(3);
+          },
         ),
       ),
+      Expanded(
+        child: FlatButton(
+          color: Colors.green,
+          onPressed: () {
+            playSound(4);
+          },
+        ),
+      ),
+      Expanded(
+        child: FlatButton(
+          color: Colors.yellow,
+          onPressed: () {
+            playSound(5);
+          },
+        ),
+      ),
+      Expanded(
+        child: FlatButton(
+          color: Colors.orange,
+          onPressed: () {
+            playSound(6);
+          },
+        ),
+      ),
+      Expanded(
+        child: FlatButton(
+          color: Colors.red,
+          onPressed: () {
+            playSound(7);
+          },
+        ),
+      ),
+
+      ],
+    ),)
+    ,
+    )
+    ,
     );
   }
 }
